1c7fcf25101fe521a749f966a7048aa5048b6ce6
Terrance Corley <terrancecorley@outlook.com>
2018-04-20 09:48:50 -0700

    refactored functions in testing to ES5

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 
 const fizzBuzzer = require('../fizzBuzzer');
 
-describe('fizzBuzzer', () => {
+describe('fizzBuzzer', function() {
   it('should return "fizz-buzz" for multiples of 15', function() {
     [15, 30, 45].forEach(function(input) {
       expect(fizzBuzzer(input)).to.equal('fizz-buzz');
